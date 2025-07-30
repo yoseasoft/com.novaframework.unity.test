d9b040d8629481be0261550bedfcd4f1296672d7
hurley <yoseasoft@163.com>
2025-07-30 20:24:25 +0800

    部署 test 模块；

---
+++ TestObject.cs
@@ -0,0 +1,14 @@
+
+namespace Game.Pack
+{
+    /// <summary>
+    /// 演示案例总控
+    /// </summary>
+    public static class TestObject
+    {
+        public static void TestStr(string str)
+        {
+            UnityEngine.Debug.Log(str);
+        }
+    }
+}
