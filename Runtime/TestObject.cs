1aefb50563c66a892660076e467bff2d127c708c
hurley <yoseasoft@163.com>
2025-08-01 10:18:24 +0800

    重新编写内容；

---
+++ Runtime/TestObject.cs
@@ -0,0 +1,16 @@
+
+using UnityEngine;
+
+namespace Game.Pack
+{
+    /// <summary>
+    /// 演示案例总控
+    /// </summary>
+    public class TestObject : MonoBehaviour
+    {
+        protected virtual void Awake()
+        {
+            Debug.LogWarning("test object awake !!!");
+        }
+    }
+}
